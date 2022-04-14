07847cc0795f19910e225a7324cc9c367dc15c84
이준규 <103472310+ljk0071@users.noreply.github.com>
2022-04-14 18:04:57 +0900

    Update Ex20.java

--- src/com/javaex/ex03/Ex20.java
+++ src/com/javaex/ex03/Ex20.java
@@ -6,19 +6,17 @@ public class Ex20 {
 
 	public static void main(String[] args) {
 		System.out.print("500원 갯수: ");
-		Scanner five = new Scanner(System.in);
-		int a = five.nextInt();
+		Scanner coin = new Scanner(System.in);
+		int five = coin.nextInt();
 		System.out.print("100원 갯수: ");
-		Scanner hund = new Scanner(System.in);
-		int b = hund.nextInt();
+		int hund = coin.nextInt();
 		System.out.print("50원 갯수: ");
-		Scanner fifty = new Scanner(System.in);
-		int c = fifty.nextInt();
+		int fifty = coin.nextInt();
 		System.out.print("10원 갯수: ");
-		Scanner ten = new Scanner(System.in);
-		int d = ten.nextInt();
-		int result = (a*500) + (b*100) + (c*50) + (d*10);
+		int ten = coin.nextInt();
+		int result = (five*500) + (hund*100) + (fifty*50) + (ten*10);
 		System.out.println("동전의 총합은 "+ result +" 원 입니다.");
+                coin.close();
 		
 	}
 
