660d25eaa4e47a3aafba348082066419f05573a7
이준규 <103472310+ljk0071@users.noreply.github.com>
2022-04-15 09:10:56 +0900

    1.1

--- src/com/javaex/ex03/Ex14.java
+++ src/com/javaex/ex03/Ex14.java
@@ -10,14 +10,12 @@ public class Ex14 {
 		Scanner a = new Scanner(System.in);
 		width = a.nextInt();
 		System.out.print("세로를 입력하세요: ");
-		Scanner b = new Scanner(System.in);
-		height = b.nextInt();
+		height = a.nextInt();
 		circ = (2*width) + (2*height);
 		area = width * height;
 		System.out.println("사각형의 넓이는: " + area);
 		System.out.println("사각형의 둘레는: " + circ);
 		a.close();
-		b.close();
 		
 	}
 
--- src/com/javaex/ex03/Ex16.java
+++ src/com/javaex/ex03/Ex16.java
@@ -6,19 +6,18 @@ public class Ex16 {
 
 	public static void main(String[] args) {
 		System.out.print("상품가격: ");
-		Scanner price = new Scanner(System.in);
-		double a = price.nextDouble();
+		Scanner a = new Scanner(System.in);
+		double price = a.nextDouble();
 		System.out.print("받은 돈: ");
-		Scanner cash = new Scanner(System.in);
-		double b = cash.nextDouble();
+		double money = a.nextDouble();
 		System.out.println("===================");
-		System.out.println("받은 돈: " + b);
-		System.out.println("상품 가격: " + a);
-		System.out.println("부가세: " + a/10);
-		System.out.println("잔액: " + (b-a));
+		System.out.println("받은 돈: " + money);
+		System.out.println("상품 가격: " + price);
+		System.out.println("부가세: " + price/10);
+		System.out.println("잔액: " + (money-price));
 		
-		price.close();
-		cash.close();
+		a.close();
+		
 		
 	}
 
