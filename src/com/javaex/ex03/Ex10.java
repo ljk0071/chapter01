0bd8a89bfafc2965dd99d9fda34c429119acb75a
이준규 <103472310+ljk0071@users.noreply.github.com>
2022-04-15 11:57:50 +0900

    1.4

--- src/com/javaex/ex03/Ex10.java
+++ src/com/javaex/ex03/Ex10.java
@@ -10,15 +10,19 @@ public class Ex10 {
 		int course = a.nextInt();
 		
 		switch (course) {
-			case 1 : System.out.println("R101호");
-			break;
-			case 2 : System.out.println("R202호");
-			break;
-			case 3 : System.out.println("R303호");
-			break;
-			case 4 : System.out.println("R404호");
-			break;
-				default :
+			case 1 : 
+				System.out.println("R101호");
+				break;
+			case 2 : 
+				System.out.println("R202호");
+				break;
+			case 3 : 
+				System.out.println("R303호");
+				break;
+			case 4 : 
+				System.out.println("R404호");
+				break;
+			default :
 				System.out.println("상담원에게 문의하세요");
 				break;
 		}
